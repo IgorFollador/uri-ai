d78f944dd96e4311baadb186efefd1d96396cf40
Igor Follador <igorledf@hotmail.com>
2023-03-07 20:53:57 -0300

    Chatbot with logic adapters

---
+++ chatbots/2_adapters_bot.py
@@ -0,0 +1,21 @@
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+# Create a new Chatbot called FolaBot
+chatbot = ChatBot('FolaBot',
+        logic_adapters=[
+            "chatterbot.logic.MathematicalEvaluation",
+            "chatterbot.logic.TimeLogicAdapter"
+        ])
+
+# Loop for the conversation to happen
+while True:
+    # Input user data
+    answer = input("User: ");
+    # Search a FolaBot response
+    response = chatbot.get_response(answer)
+
+    if float(response.confidence) > 0.5:
+        print('FolaBot: ', response)
+    else:
+        print("FolaBot: I`m sorry! I don`t have response to your answer.")
